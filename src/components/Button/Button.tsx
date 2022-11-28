95c4edc7b0b453bffada1fc0b3dbf7a108b0817a
erezabegu <ereza@dua.com>
2022-11-28 20:38:21 +0100

    test

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+export interface ButtonProps {
+    label: string;
+}
+
+const Button = ({label}: ButtonProps) => {
+    return <button>{label}</button>
+}
+export default Button
